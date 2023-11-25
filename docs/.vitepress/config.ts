3cb6b3374f90c65b8e2f2b917721d977d07e366d
tanx <tanx@landray.com.cn>
2023-11-25 14:46:12 +0800

    更换icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Conyu blog",
-  head: [['link', { rel: 'icon', href: 'icon/feather.ico' }]],
+  head: [['link', { rel: 'icon', href: 'icon/feather2.ico' }]],
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
