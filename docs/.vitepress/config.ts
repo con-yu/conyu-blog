6ed89ad1465f8437db765cecdc567f99bc8a8dab
tanx <tanx@landray.com.cn>
2023-11-25 14:40:54 +0800

    修改静态文件夹名称、更换icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Conyu blog",
-  head: [['link', { rel: 'icon', href: 'icons/write.ico' }]],
+  head: [['link', { rel: 'icon', href: 'icon/feather.ico' }]],
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '首页', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
       { text: '前端笔记', link: '/frontend/Module/index' },
       { text: '杂项', link: '/frontend/regexp/Patterns&flags' }
