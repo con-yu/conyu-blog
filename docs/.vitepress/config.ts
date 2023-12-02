c8616f4caac8d9dcdaa031fda6c7cf7f742fd10d
tanx <tanx@landray.com.cn>
2023-12-02 18:10:22 +0800

    优化目录结构

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,30 +2,37 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "从羽谈",
+  title: '从羽谈',
   head: [['link', { rel: 'icon', href: 'icon/write.ico' }]],
-  description: "A VitePress Site",
+  description: 'A VitePress Site',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '首页 🏠', link: '/' },
+      { text: '测试特性 🤖', link: '/test' },
       { text: '示例 ✈', link: '/markdown-examples' },
-      { text: '前端笔记 🌈', link: '/frontend/Module/index' },
-      { text: '杂项 📦', link: '/frontend/regexp/Patterns&flags' }
+      { text: '前端笔记 🌈', link: 'view/frontend/Module/index' },
+      { text: '杂项 📦', link: 'view/frontend/regexp/Patterns&flags' },
     ],
-
     sidebar: [
       {
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' },
-        ]
-      }
+        ],
+      },
     ],
+    // 广告
+    carbonAds: {
+      code: 'your-carbon-code as',
+      placement: 'your-carbon-placement',
+    },
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
+  lastUpdated: true,
+  markdown: {
+    lineNumbers: true,
   },
 })
